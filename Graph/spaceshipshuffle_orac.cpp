6b0b4a8521ee2ea8be88032399c34f704f6ae77b
YichenOl <dyc161302@gmail.com>
2026-03-22 21:35:43 +0800

    Update spaceshipshuffle_orac.cpp
    This is a big brain problem("-ࡇ-)
    So the key observation is that in the loop of movements between the spaceships, there is always two cabins where no one moves between them:
    Prove: 
    say if the chain of movements between some cabins are like this (sample testcase 1):
    5->(3)->2->(2)->1->(1)->6->(0)->5->(2)->1->(1)->back to cabin 0
    Notice that at cabin 1 of 2 ppl, 3 ppl move in and 2 moves out, the total movement is 5 and the cabin gets one ppl, however, if 1 person move in and 0 moves out, the cabin still gets one but total movement is only one, if 0 move in and -1 moves out, the cabin still gets one and total movement is only one, and so on...
    
    Hence, the problem becomes one that asks us to minimise the movement so that the change in each cabin remains the same. 
    
    Notice that I did not mention how the first string of movement is derived because when the cabin in a line shape and not donut shape, calculating the required movement is easy as the movement between any two cabin is the difference between the sum of original number of ppl and the sum of the required number of people on the left or the right portion, but when the cabins are donut-shaped, there is no left or right portion. However, from our previous observation, we can deduce that the most optimal movement is when the number of positive and negative movements are equivalent, which means that one of the optimal movements will always contain a zero, and we can safely break at any point we want, as long as we find the median of the movements and increment/decrement all movements until the median reaches 0:D

--- Graph/spaceshipshuffle_orac.cpp
+++ Graph/spaceshipshuffle_orac.cpp
@@ -5,7 +5,7 @@ typedef long long ll;
 #define fi first
 #define se second
 #define int ll
-//partial
+//ACed finally("-ࡇ-)
 int32_t main() {
 	int n; cin >> n;
 	ll ans = 0;
@@ -13,13 +13,18 @@ int32_t main() {
 	vector<int> b(n);
 	for(int i=0; i<n; i++) cin >> a[i];
 	for(int i=0; i<n; i++) cin >> b[i];
-	int x=(n-1)/2;
-	int y=n-1-x;
-	for(int i=1; i<=x; i++) {
-		ans += i*b[i];
+	
+	vector<int> move(n-1);
+	move[0]=a[0]-b[0];
+	for(int i=1; i<n-1; i++) {
+		move[i]=a[i]-b[i]+move[i-1];
 	}
-	for(int i=x+1; i<n; i++) {
-		ans += (n-i)*b[i];
+	
+	move.push_back(0);
+	sort(move.begin(), move.end());
+	int k = move[(n)/2+((n)%2)-1];
+	for(auto i:move) {
+		ans += abs(i-k);
 	}
 	cout << ans;
-}	
+}
